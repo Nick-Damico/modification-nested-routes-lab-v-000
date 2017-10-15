18897db418ecfe972eb2ef1e653ce4aefe620784
Nicholas D'Amico <nickalan82@icloud.com>
2017-10-15 15:54:37 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :artists do
-    resources :songs, only: [:index, :show]
+    resources :songs, only: [:index, :show, :new, :create, :edit]
   end
   resources :songs
 end
+
+# artist_songs GET    /artists/:artist_id/songs(.:format)     songs#index
+# POST   /artists/:artist_id/songs(.:format)     songs#create
+# new_artist_song GET    /artists/:artist_id/songs/new(.:format) songs#new
+# artist_song GET    /artists/:artist_id/songs/:id(.:format) songs#show
